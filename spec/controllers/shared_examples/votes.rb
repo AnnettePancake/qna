f815e453e2ccaff3bbfcb52acff02d6e8dd12431
Anna <annette.pancake@gmail.com>
2017-01-18 14:57:19 +0300

    Added checking correct value for controller spec votes

--- spec/controllers/shared_examples/votes.rb
+++ spec/controllers/shared_examples/votes.rb
@@ -5,18 +5,22 @@ shared_examples 'votes' do
   sign_in_user
 
   describe 'POST #like' do
-    it 'saves like in the database' do
+    it 'saves like in the database with correct value' do
       expect do
         post :like, params: { id: voteable.id }, format: :js
       end.to change(voteable.votes, :count).by(1)
+
+      expect(voteable.votes.last.value).to eq 1
     end
   end
 
   describe 'POST #dislike' do
-    it 'saves dislike in the database' do
+    it 'saves dislike in the database with correct value' do
       expect do
         post :dislike, params: { id: voteable.id }, format: :js
       end.to change(voteable.votes, :count).by(1)
+
+      expect(voteable.votes.last.value).to eq -1
     end
   end
 end
