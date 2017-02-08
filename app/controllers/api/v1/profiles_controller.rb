682d96eddb8107a0b5edac6b4eac5873a67393b4
Anna <annette.pancake@gmail.com>
2017-02-09 02:53:48 +0300

    Add api for users list

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,17 +1,26 @@
-class Api::V1::ProfilesController < ApplicationController
-  before_action :doorkeeper_authorize!
+# frozen_string_literal: true
+module Api
+  module V1
+    class ProfilesController < ApplicationController
+      before_action :doorkeeper_authorize!
 
-  skip_authorization_check
+      skip_authorization_check
 
-  respond_to :json
+      respond_to :json
 
-  def me
-    respond_with current_resource_owner
-  end
+      def me
+        respond_with current_resource_owner
+      end
 
-  protected
+      def list
+        respond_with(User.where.not(id: current_resource_owner.id))
+      end
 
-  def current_resource_owner
-    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+      protected
+
+      def current_resource_owner
+        @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+      end
+    end
   end
 end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -5,12 +5,12 @@ describe 'Profile API' do
   describe 'GET /me' do
     context 'unauthorized' do
       it 'returns 401 status if there is no access_token' do
-        get '/api/v1/profiles/me', format: :json
+        get '/api/v1/profiles/me', params: { format: :json }
         expect(response.status).to eq 401
       end
 
       it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/profiles/me', format: :json, access_token: '1234'
+        get '/api/v1/profiles/me', params: { format: :json, access_token: '1234' }
         expect(response.status).to eq 401
       end
     end
@@ -19,7 +19,10 @@ describe 'Profile API' do
       let(:me) { create(:user) }
       let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
-      before { get '/api/v1/profiles/me', format: :json, access_token: access_token.token }
+      before do
+        get '/api/v1/profiles/me', params: { format: :json,
+                                             access_token: access_token.token }
+      end
 
       it 'returns 200 status' do
         expect(response).to be_success
@@ -27,20 +30,52 @@ describe 'Profile API' do
 
       %w(id email created_at updated_at admin).each do |attr|
         it "contains #{attr}" do
-          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path("#{attr}")
+          expect(response.body).to be_json_eql(me.send(attr.to_sym).to_json).at_path(attr.to_s)
         end
       end
 
       %w(password encrypted_password).each do |attr|
         it "does not contain #{attr}" do
-          expect(response.body).not_to have_json_path("#{attr}")
+          expect(response.body).not_to have_json_path(attr.to_s)
         end
       end
+    end
+  end
 
-
-      it 'does not contain encrypted_password' do
-        expect(response.body).not_to have_json_path('encrypted_password')
+  describe 'GET /list' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/profiles/list', params: { format: :json }
+        expect(response.status).to eq 401
       end
+
+      it 'returns 401 status if access_token is invalid' do
+        get '/api/v1/profiles/list', params: { format: :json, access_token: '1234' }
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  context 'authorized' do
+    let(:me) { create(:user) }
+    let(:users_list) { create_pair(:user) }
+    let(:access_token) { create(:access_token, resource_owner_id: me.id) }
+
+    before do
+      get '/api/v1/profiles/list', params: { format: :json,
+                                             access_token: access_token.token }
+    end
+
+    it 'returns 200 status' do
+      expect(response).to be_success
+    end
+
+    it 'contains list of users except me' do
+      expect(response.body).not_to be_json_eql(users_list.to_json)
+    end
+
+    it 'does not contain profile of me' do
+      expect(response.body).not_to include_json(me.to_json)
     end
   end
 end
