7a14f4066aaead3d391b53727252cd5d5187a7a7
Anna <annette.pancake@gmail.com>
2017-02-10 15:32:56 +0300

    Refactor profiles controller

--- config/routes.rb
+++ config/routes.rb
@@ -30,9 +30,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resource :profiles do
+      resources :profiles, only: :index do
         get :me, on: :collection
-        get :list, on: :collection
       end
     end
   end
