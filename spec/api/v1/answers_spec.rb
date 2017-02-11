f588424c2cf5b7564eac180728be6c4eea06de71
Anna <annette.pancake@gmail.com>
2017-02-11 16:47:42 +0300

    Add shared examples for api

--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -5,174 +5,66 @@ describe 'Answers API' do
   describe 'GET /index' do
     let!(:question) { create(:question) }
 
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get "/api/v1/questions/#{question.id}/answers", params: { format: :json }
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get "/api/v1/questions/#{question.id}/answers", params: { format: :json,
-                                                                  access_token: '1234' }
-        expect(response.status).to eq 401
-      end
-    end
+    it_behaves_like 'API Authenticable'
 
     context 'authorized' do
       let(:access_token) { create(:access_token) }
       let!(:answers) { create_pair(:answer, question: question) }
       let(:answer) { answers.first }
+      let(:answer_path) { '0/' }
 
       before do
         get "/api/v1/questions/#{question.id}/answers",
             params: { format: :json, access_token: access_token.token }
       end
 
-      it 'returns 200 status code' do
-        expect(response).to be_success
-      end
+      it_behaves_like 'API Status 200'
+      it_behaves_like 'API List'
+      it_behaves_like 'API Answer attributes'
+    end
 
-      it 'returns list of answers' do
-        expect(response.body).to have_json_size(2)
-      end
-
-      %w(id body created_at updated_at question_id).each do |attr|
-        it "answer object contains #{attr}" do
-          expect(response.body)
-            .to be_json_eql(answer.send(attr.to_sym).to_json)
-            .at_path("0/#{attr}")
-        end
-      end
+    def do_request(options = {})
+      get "/api/v1/questions/#{question.id}/answers", { format: :json }.merge(options)
     end
   end
 
   describe 'GET /show' do
     let(:question) { create(:question) }
     let(:answer) { create(:answer, question: question) }
+    let!(:comment) { create(:comment, commentable: answer) }
+    let!(:attachment) { create(:attachment, attachable: answer) }
+    let(:answer_path) { '' }
 
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get "/api/v1/questions/#{question.id}/answers/#{answer.id}", params: { format: :json }
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get "/api/v1/questions/#{question.id}/answers/#{answer.id}",
-            params: { format: :json, access_token: '1234' }
-        expect(response.status).to eq 401
-      end
-    end
+    it_behaves_like 'API Authenticable'
 
     context 'authorized' do
       let(:access_token) { create(:access_token) }
-      let!(:comment) { create(:comment, commentable: answer) }
-      let!(:attachment) { create(:attachment, attachable: answer) }
 
       before do
         get "/api/v1/questions/#{question.id}/answers/#{answer.id}",
             params: { format: :json, access_token: access_token.token }
       end
 
-      it 'returns 200 status code' do
-        expect(response).to be_success
-      end
+      it_behaves_like 'API Commentable'
+      it_behaves_like 'API Attachable'
+      it_behaves_like 'API Status 200'
+      it_behaves_like 'API Answer attributes'
+    end
 
-      %w(id body created_at updated_at question_id).each do |attr|
-        it "answer object contains #{attr}" do
-          expect(response.body)
-            .to be_json_eql(answer.send(attr.to_sym).to_json)
-            .at_path(attr.to_s)
-        end
-      end
-
-      context 'comments' do
-        it 'included in answer object' do
-          expect(response.body).to have_json_size(1).at_path('comments')
-        end
-
-        %w(id body created_at updated_at commentable_id commentable_type).each do |attr|
-          it "contains #{attr}" do
-            expect(response.body)
-              .to be_json_eql(comment.send(attr.to_sym).to_json)
-              .at_path("comments/0/#{attr}")
-          end
-        end
-      end
-
-      context 'attachments' do
-        it 'included in answer object' do
-          expect(response.body).to have_json_size(1).at_path('attachments')
-        end
-
-        %w(id file created_at updated_at attachable_id attachable_type).each do |attr|
-          it "contains #{attr}" do
-            expect(response.body)
-              .to be_json_eql(attachment.send(attr.to_sym).to_json)
-              .at_path("attachments/0/#{attr}")
-          end
-        end
-
-        it 'contains attachment url' do
-          expect(response.body)
-            .to be_json_eql(attachment.file.url.to_json)
-            .at_path('attachments/0/url')
-        end
-      end
+    def do_request(options = {})
+      get "/api/v1/questions/#{question.id}/answers/#{answer.id}", { format: :json }.merge(options)
     end
   end
 
   describe 'GET /create' do
     let(:question) { create(:question) }
+    let(:path) { "#{question.id}/answers" }
 
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get "/api/v1/questions/#{question.id}/answers", params: { format: :json }
-        expect(response.status).to eq 401
-      end
+    it_behaves_like 'API Authenticable'
+    it_behaves_like 'API Create object', :answer
 
-      it 'returns 401 status if access_token is invalid' do
-        get "/api/v1/questions/#{question.id}/answers", params: { format: :json,
-                                                                  access_token: '1234' }
-        expect(response.status).to eq 401
-      end
-    end
-
-    context 'authorized' do
-      let!(:access_token) { create(:access_token) }
-
-      context 'valid params' do
-        it 'returns 201 status code' do
-          post "/api/v1/questions/#{question.id}/answers",
-               params: { format: :json, access_token: access_token.token,
-                         answer: attributes_for(:answer) }
-          expect(response.status).to eq(201)
-        end
-
-        it 'saves new answer in the database' do
-          expect do
-            post "/api/v1/questions/#{question.id}/answers",
-                 params: { format: :json, access_token: access_token.token,
-                           answer: attributes_for(:answer) }
-          end.to change(Answer, :count).by(1)
-        end
-      end
-
-      context 'invalid params' do
-        it 'returns 422 status code' do
-          post "/api/v1/questions/#{question.id}/answers",
-               params: { format: :json, access_token: access_token.token,
-                         answer: attributes_for(:invalid_answer) }
-          expect(response.status).to eq(422)
-        end
-
-        it 'does not save new answer in the database' do
-          expect do
-            post "/api/v1/questions/#{question.id}/answers",
-                 params: { format: :json, access_token: access_token.token,
-                           answer: attributes_for(:invalid_answer) }
-          end.not_to change(Answer, :count)
-        end
-      end
+    def do_request(options = {})
+      get "/api/v1/questions/#{question.id}/answers", { format: :json }.merge(options)
     end
   end
 end
---
+++ spec/support/shared/api/api_comment.rb
@@ -0,0 +1,16 @@
+shared_examples_for 'API Commentable' do
+  context 'comments' do
+
+    it "included in object" do
+      expect(response.body).to have_json_size(1).at_path('comments')
+    end
+
+    %w(id body created_at updated_at commentable_id commentable_type).each do |attr|
+      it "contains #{attr}" do
+        expect(response.body)
+          .to be_json_eql(comment.send(attr.to_sym).to_json)
+          .at_path("comments/0/#{attr}")
+      end
+    end
+  end
+end
