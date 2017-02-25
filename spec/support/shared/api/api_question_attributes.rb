d6b53c69b9dc201f7e8a2d21948caf1e01660bb5
Anna <annette.pancake@gmail.com>
2017-02-25 04:22:56 +0300

    Add cache, rspec fix

--- spec/support/shared/api/api_question_attributes.rb
+++ spec/support/shared/api/api_question_attributes.rb
@@ -3,7 +3,7 @@ shared_examples 'API Question attributes' do
   %w(id title body created_at updated_at).each do |attr|
     it "question object contains #{attr}" do
       expect(response.body)
-        .to be_json_eql(question.send(attr.to_sym).to_json)
+        .to be_json_eql(question.reload.send(attr.to_sym).to_json)
         .at_path("#{path}#{attr}")
     end
   end
